8187654b53e0897c49b16d96da41cf11e41bda4e
fangertao <fetaoily@foxmail.com>
2018-02-05 22:12:54 +0800

    Arcade Demo001

---
+++ examples/Arcade/demo_001.js
@@ -0,0 +1,33 @@
+let game;
+let sprite;
+
+window.onload = () => {
+  game = new Phaser.Game(640, 480, Phaser.AUTO);
+  game.state.add('PlayGame', PlayGame);
+  game.state.start('PlayGame');
+};
+
+let PlayGame = function () {
+};
+
+PlayGame.prototype = {
+  preload: () => {
+    game.load.image('arrow', 'assets/sprites/arrow.png');
+  },
+  create: () => {
+    game.backgroundColor = '#222222';
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    sprite = game.add.sprite(400, 300, 'arrow');
+    sprite.anchor.setTo(0.5, 0.5);
+
+    game.physics.enable(sprite, Phaser.Physics.ARCADE);
+    sprite.body.allowRotation = false;
+  },
+  update: () => {
+    sprite.rotation = game.physics.arcade.moveToPointer(sprite, 60, game.input.activePointer, 500);
+  },
+  render: () => {
+    game.debug.spriteInfo(sprite, 32, 32);
+  }
+};
