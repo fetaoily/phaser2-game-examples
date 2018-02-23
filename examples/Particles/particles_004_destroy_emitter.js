4339c72308f897c4dcef349d89e35c10cb21b811
fetaoily <fetaoily@gmail.com>
2018-02-23 15:34:51 +0800

    Particles particles_004_destroy_emitter.js

---
+++ examples/Particles/particles_004_destroy_emitter.js
@@ -0,0 +1,47 @@
+let game;
+
+window.onload = () => {
+  game = new Phaser.Game(800, 600, Phaser.AUTO);
+  game.state.add('PlayGame', PlayGame);
+  game.state.start('PlayGame');
+};
+
+let PlayGame = function () {
+};
+
+PlayGame.prototype = {
+  preload () {
+    //
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    //
+    game.load.image('diamond', '/assets/sprites/diamond.png');
+  },
+  create () {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    //
+    game.stage.backgroundColor = 0x337799;
+    //
+    this.emitter = game.add.emitter(0, 0, 100);
+    this.emitter.makeParticles('diamond');
+    this.emitter.gravity = 200;
+    //
+    game.input.onDown.addOnce(this.particleBurst, this);
+  },
+  update () {
+  },
+  render () {
+  },
+  particleBurst (pointer) {
+    //
+    this.emitter.x = pointer.x;
+    this.emitter.y = pointer.y;
+    this.emitter.start(true, 4000, null, 10);
+    //
+    game.time.events.add(2000, this.destroyEmitter, this);
+  },
+  destroyEmitter () {
+    this.emitter.destroy();
+  }
+};
