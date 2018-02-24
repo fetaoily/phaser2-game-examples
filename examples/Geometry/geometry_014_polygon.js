16c6fdccf69097ea890c2c371ca7a3cf9342ed0c
fetaoily <fetaoily@gmail.com>
2018-02-24 16:19:38 +0800

    Geometry geometry_014_polygon.js

--- examples/Geometry/geometry_014_polygon.js
+++ examples/Geometry/geometry_014_polygon.js
@@ -1,5 +1,6 @@
 let game;
 
+let colors;
 
 window.onload = () => {
   game = new Phaser.Game(800, 600, Phaser.AUTO);
@@ -17,16 +18,69 @@ PlayGame.prototype = {
     game.scale.pageAlignVertically = true;
   },
   create () {
+    //
+    colors = this.colors = Phaser.Color.HSVColorWheel();
+    // colors = this.colors = Phaser.Color.HSLColorWheel();
+    //
     this.poly = new Phaser.Polygon();
     this.poly.setTo([new Phaser.Point(200, 100), new Phaser.Point(350, 100), new Phaser.Point(375, 200), new Phaser.Point(150, 200)]);
+    this.drawPolygon(this.poly.points);
     //
-    this.graphics = game.add.graphics(0,0);
-    this.graphics.beginFill(0xff33ff);
-    this.graphics.drawPolygon(this.poly.points);
-    this.graphics.endFill();
+    this.poly = new Phaser.Polygon([new Phaser.Point(game.world.randomX, game.world.randomY), new Phaser.Point(game.world.randomX, game.world.randomY), new Phaser.Point(game.world.randomX, game.world.randomY), new Phaser.Point(game.world.randomX, game.world.randomY)]);
+    this.drawPolygon(this.poly);
+    //
+    this.poly = new Phaser.Polygon(new Phaser.Point(game.world.randomX, game.world.randomY), new Phaser.Point(game.world.randomX, game.world.randomY), new Phaser.Point(game.world.randomX, game.world.randomY), new Phaser.Point(game.world.randomX, game.world.randomY));
+    this.drawPolygon(this.poly);
+    //
+    this.poly = new Phaser.Polygon(game.world.randomX, game.world.randomY, game.world.randomX, game.world.randomY, game.world.randomX, game.world.randomY, game.world.randomX, game.world.randomY);
+    this.drawPolygon(this.poly);
+    //
+    this.poly = new Phaser.Polygon([game.world.randomX, game.world.randomY, game.world.randomX, game.world.randomY, game.world.randomX, game.world.randomY, game.world.randomX, game.world.randomY, game.world.randomX, game.world.randomY]);
+    this.drawPolygon(this.poly);
+    //
+    this.poly = new Phaser.Polygon(
+        {x: game.world.randomX, y: game.world.randomY},
+        {x: game.world.randomX, y: game.world.randomY},
+        {x: game.world.randomX, y: game.world.randomY},
+        {x: game.world.randomX, y: game.world.randomY}
+    );
+    this.drawPolygon(this.poly);
+    //
+    this.poly = new Phaser.Polygon([
+      {x: game.world.randomX, y: game.world.randomY},
+      {x: game.world.randomX, y: game.world.randomY},
+      {x: game.world.randomX, y: game.world.randomY},
+      {x: game.world.randomX, y: game.world.randomY}
+    ]);
+    this.drawPolygon(this.poly);
+    //
+    this.poly = new Phaser.Polygon(
+        new Phaser.Point(game.world.randomX, game.world.randomY),
+        {x: game.world.randomX, y: game.world.randomY},
+        game.world.randomX,
+        game.world.randomY,
+        new Phaser.Point(game.world.randomX, game.world.randomY)
+    );
+    this.drawPolygon(this.poly);
+    //
+    this.poly = new Phaser.Polygon([
+      new Phaser.Point(game.world.randomX, game.world.randomY),
+      {x: game.world.randomX, y: game.world.randomY},
+      game.world.randomX,
+      game.world.randomY,
+      new Phaser.Point(game.world.randomX, game.world.randomY)
+    ]);
+    this.drawPolygon(this.poly);
   },
   update () {
   },
   render () {
+  },
+  drawPolygon (points) {
+    this.graphics = game.add.graphics(0, 0);
+    // this.graphics.beginFill(0xff33ff);
+    this.graphics.beginFill(this.colors[game.rnd.integerInRange(0, 359)].color);
+    this.graphics.drawPolygon(points);
+    this.graphics.endFill();
   }
 };
