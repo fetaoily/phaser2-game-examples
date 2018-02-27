b8388a2f67d542107c5f906ceb5a092c320d151f
fetaoily <fetaoily@gmail.com>
2018-02-27 08:31:35 +0800

    Debug debug_003_debug_display.js

---
+++ examples/Debug/debug_003_debug_display.js
@@ -0,0 +1,40 @@
+let game;
+
+window.onload = () => {
+  game = new Phaser.Game(800, 600, Phaser.AUTO);
+  game.state.add('PlayGame', PlayGame);
+  game.state.start('PlayGame');
+};
+
+let PlayGame = function () {
+};
+
+PlayGame.prototype = {
+  preload () {
+    //
+    this.sprite = null;
+    this.counter = 0;
+    this.step = Math.PI * 2 / 360;
+    //
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    //
+    game.load.image('sprite', '/assets/sprites/phaser2.png');
+  },
+  create () {
+    this.sprite = game.add.sprite(0, 0, 'sprite');
+    this.sprite.alpha = 0.5;
+    this.sprite.x = game.width / 2;
+    this.sprite.anchor.x = this.sprite.anchor.y = 0.5;
+  },
+  update () {
+    this.tStep = Math.sin(this.counter);
+    this.sprite.y = (game.height / 2) + this.tStep * 30;
+    this.sprite.previousRotation += Phaser.Math.degToRad(0.1 * this.tStep);
+    this.counter += this.step;
+  },
+  render () {
+    game.debug.spriteBounds(this.sprite);
+  }
+};
