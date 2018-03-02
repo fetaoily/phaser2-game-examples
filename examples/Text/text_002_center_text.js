c671f0df3e4b743a16656a25894a132c8cf043f6
fetaoily <fetaoily@gmail.com>
2018-03-02 17:46:51 +0800

    Text text_002_center_text.js

---
+++ examples/Text/text_002_center_text.js
@@ -0,0 +1,37 @@
+(() => {
+  let game;
+
+  window.onload = () => {
+    game = new Phaser.Game(800, 600, Phaser.AUTO);
+    game.state.add('PlayGame', PlayGame);
+    game.state.start('PlayGame');
+  };
+
+  let PlayGame = function () {
+  };
+
+  PlayGame.prototype = {
+    preload () {
+      game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      game.scale.pageAlignHorizontally = true;
+      game.scale.pageAlignVertically = true;
+      //
+      game.load.image('bg', '/assets/skies/deepblue.png');
+    },
+    create () {
+      game.add.image(0, 0, 'bg');
+      this.bar = game.add.graphics();
+      this.bar.beginFill(0x000000, 0.2);
+      this.bar.drawRect(0, 100, 800, 100);
+      //
+      let style = {font: 'bold 32px Arial', fill: '#ffffff', boundsAlignH: 'center', boundsAlignV: 'middle'};
+      this.text = game.add.text(0, 0, 'Phaser 2.4 text bounds', style);
+      this.text.setShadow(3, 3, 'rgb(0,0,0,0.5)', 2);
+      this.text.setTextBounds(0, 100, 800, 100);
+    },
+    update () {
+    },
+    render () {
+    }
+  };
+})();
