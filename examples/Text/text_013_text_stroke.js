9cbc616edc8650db2409584573c09dd66ff1d380
fangertao <fetaoily@foxmail.com>
2018-03-04 13:08:18 +0800

    Text text_013_text_stroke.js

---
+++ examples/Text/text_013_text_stroke.js
@@ -0,0 +1,52 @@
+(() => {
+  let game;
+
+  window.onload = () => {
+    game = new NewGame();
+  };
+
+  class NewGame extends Phaser.Game {
+    constructor () {
+      super();
+      this.loadStates();
+    }
+
+    loadStates () {
+      this.state.add('PlayGame', PlayGame);
+      this.state.start('PlayGame');
+    }
+  }
+
+  class PlayGame extends Phaser.State {
+    constructor () {
+      super();
+    }
+
+    preload () {
+      this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      this.scale.pageAlignHorizontally = true;
+      this.scale.pageAlignVertically = true;
+    }
+
+    create () {
+      this.stage.setBackgroundColor(0x2d2d2d);
+      //
+      this.text = this.add.text(this.world.centerX, this.world.centerY,'- phaser text stroke -');
+      this.text.anchor.set(0.5);
+      this.text.align = 'center';
+      this.text.font = 'Arial Black';
+      this.text.fontSize = 50;
+      this.text.fontWeight = 'bold';
+      //
+      this.stroke = '#000000';
+      this.text.strokeThickness = 30;
+      this.text.fill = '#43d637';
+    }
+
+    update () {
+    }
+
+    render () {
+    }
+  }
+})();
