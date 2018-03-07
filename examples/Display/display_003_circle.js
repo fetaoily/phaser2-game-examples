0408cdb3583d96c34533d8ccb2f05d221c3118cb
fetaoily <fetaoily@gmail.com>
2018-03-07 19:21:50 +0800

    Animation display_003_circle.js

---
+++ examples/Display/display_003_circle.js
@@ -0,0 +1,39 @@
+(() => {
+  let game;
+
+  window.onload = () => {
+    game = new NewGame();
+  };
+
+  class NewGame extends Phaser.Game {
+    constructor () {
+      super(800, 600, Phaser.AUTO);
+      this.state.add('PlayGame', PlayGame);
+      this.state.start('PlayGame');
+    }
+  }
+
+  class PlayGame extends Phaser.State {
+    constructor () {
+      super();
+    }
+
+    preload () {
+      this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      this.scale.pageAlignHorizontally = true;
+      this.scale.pageAlignVertically = true;
+    }
+
+    create () {
+      this.graphics = this.add.graphics(0, 0);
+      this.graphics.beginFill(0xff0000, 1);
+      this.graphics.drawCircle(300, 300, 100);
+    }
+
+    update () {
+    }
+
+    render () {
+    }
+  }
+})();
