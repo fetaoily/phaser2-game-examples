34167038f33a0ec6549a5eb3d7d9b612b9642597
fetaoily <fetaoily@gmail.com>
2018-03-07 13:51:17 +0800

    Time time_004_elapsed_seconds.js

---
+++ examples/Time/time_004_elapsed_seconds.js
@@ -0,0 +1,37 @@
+(() => {
+  let game;
+
+  window.onload = () => {
+    game = new NewGame();
+  };
+
+  class NewGame extends Phaser.Game {
+    constructor () {
+      super(800, 600, Phaser.AUTO);
+      this.state.add('PlayGame', PlayGame);
+      this.state.start('PlayGame');
+    }
+  }
+
+  class PlayGame extends Phaser.State {
+    constructor () {
+      super();
+    }
+
+    preload () {
+      this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      this.scale.pageAlignHorizontally = true;
+      this.scale.pageAlignVertically = true;
+    }
+
+    create () {
+    }
+
+    update () {
+    }
+
+    render () {
+      this.game.debug.text('Elapsed seconds: ' + this.game.time.totalElapsedSeconds().toFixed(3), 32, 32);
+    }
+  }
+})();
