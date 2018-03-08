f0d3359cdf29b73502b2a4ac850e1a20567fc5e8
fetaoily <fetaoily@gmail.com>
2018-03-08 09:18:32 +0800

    Animation display_004_ellipse.js

---
+++ examples/Display/display_004_ellipse.js
@@ -0,0 +1,51 @@
+(() => {
+  let game;
+
+  window.onload = () => {
+    game = new NewGame();
+  };
+
+  class NewGame extends Phaser.Game {
+    constructor () {
+      super(800, 600, Phaser.AUTO);
+      this.state.add('PlayGame', PlayGame);
+      this.state.start('PlayGame');
+    }
+  }
+
+  class PlayGame extends Phaser.State {
+    constructor () {
+      super();
+    }
+
+    preload () {
+      this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      this.scale.pageAlignHorizontally = true;
+      this.scale.pageAlignVertically = true;
+    }
+
+    create () {
+      this.graphics = this.add.graphics(this.world.centerX, this.world.centerY);
+      this.graphics.lineStyle(8, 0xffd900);
+      this.graphics.drawEllipse(100, 100, 200, 60);
+      //
+      this.graphics.arc(0, 0, 135, this.math.degToRad(0), this.math.degToRad(90), false);
+      //
+      this.graphics.moveTo(-100, -100);
+      //
+      this.graphics.lineStyle(0);
+      this.graphics.beginFill(0xff3300);
+      this.graphics.arc(-100, -100, 135, this.math.degToRad(0), this.math.degToRad(90), true);
+      this.graphics.endFill();
+      //
+      this.graphics.inputEnabled = true;
+      this.graphics.input.enableDrag();
+    }
+
+    update () {
+    }
+
+    render () {
+    }
+  }
+})();
